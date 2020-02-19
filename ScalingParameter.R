1d535e75d6e0d3d97120e02ee6eb0b17b5622e95
Matthew Grinnell <matthew.grinnell@dfo-mpo.gc.ca>
2020-02-19 11:41:14 -0800

    Add figures

--- ScalingParameter.R
+++ ScalingParameter.R
@@ -3,9 +3,13 @@ rm( list=ls() )
 
 # Packages
 require( tidyverse )
+require( scales )
 
 # Regions
 regions <- c( "HG", "PRD", "CC", "SoG", "WCVI" )
+
+# New survey year
+newSurvYr <- 1988
 
 # Load the spawn indices
 LoadIndices <- function( r ) {
@@ -15,20 +19,47 @@ LoadIndices <- function( r ) {
   for( i in 1:length(r) ) {
     # Import the data
     res[[i]] <- read_csv( file=paste("Spawn", r[i], ".csv", sep=""),
-                     col_types=cols() ) %>%
-    mutate( Region=r[i] )
+                          col_types=cols() ) %>%
+      mutate( Region=r[i] )
   }  # End i loop over regions
-  # Bind tibbles
+  # Bind tibbles and wrangle
   out <- bind_rows( res ) %>%
     replace_na( replace=list(Surf=0, Macro=0, Under=0) ) %>%
-    group_by( Region, Year ) %>%
-    summarise( Surface=Surf/Total*100, Dive=(Macro+Under)/Total*100 ) %>%
+    group_by( Region, Year, Total ) %>%
+    summarise( Surface=Surf/Total, Dive=(Macro+Under)/Total ) %>%
     ungroup( ) %>%
-    mutate( Region=factor(Region, levels=r) ) %>%
-    arrange( Region, Year )
+    pivot_longer( cols=c(Surface, Dive), names_to="Survey",
+                  values_to="Proportion" ) %>%
+    mutate( Index=Proportion*Total, Region=factor(Region, levels=r),
+            Survey=factor(Survey, levels=c("Surface", "Dive")) ) %>%
+    arrange( Region, Year ) %>%
+    select( -Total )
   # Return data
   return( out )
 }  # End LoadIndices function
 
 # Load spawn indices
-dat <- LoadIndices( r=regions )
+spawn <- LoadIndices( r=regions )
+
+# Figure: bar plot
+spawnProp <- ggplot( data=spawn, mapping=aes(x=Year, y=Proportion, group=Survey,
+                                            fill=Survey) ) +
+  geom_bar( stat="identity" ) + 
+  geom_vline( xintercept=newSurvYr-0.5, linetype="dashed", size=0.5 ) +
+  facet_wrap( Region ~ ., dir="v", ncol=2 ) +
+  scale_fill_viridis_d( ) +
+  theme_bw( ) +
+  theme( legend.position="top" ) +
+  ggsave( filename="SpawnProp.png", height=6, width=6 )
+
+# Figure: points
+spawnIndex <- ggplot( data=spawn, mapping=aes(x=Year, y=Index, group=Survey,
+                                            fill=Survey) ) +
+  geom_bar( stat="identity" ) +
+  geom_vline( xintercept=newSurvYr-0.5, linetype="dashed", size=0.5 ) +
+  facet_wrap( Region ~ ., dir="v", ncol=2, scales="free_y" ) +
+  scale_y_continuous( labels=comma ) +
+  scale_fill_viridis_d( ) +
+  theme_bw( ) +
+  theme( legend.position="top" ) +
+  ggsave( filename="SpawnIndex.png", height=6, width=6 )
