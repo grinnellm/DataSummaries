f3b62e4525d9a0bff24fd0aae36b9efb187e62d8
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2021-03-04 14:48:37 -0800

    Denominator is M + F (in case some aren't sexed)

--- Summary.R
+++ Summary.R
@@ -105,7 +105,7 @@ options(dplyr.summarise.inform = FALSE)
 ##### Controls #####
 
 # Select region(s): major (HG, PRD, CC, SoG, WCVI); minor (A27, A2W, JS); All
-if (!exists("region")) region <- "HG"
+if (!exists("region")) region <- "All"
 
 # Sections to include for sub-stock analyses
 SoGS <- c(173, 181, 182, 191:193)
@@ -1495,7 +1495,8 @@ propFemale <- bio %>%
   select(Year, Sex, SampWt) %>%
   na.omit() %>%
   group_by(Year) %>%
-  summarise(Proportion = SumNA(SampWt[Sex == "F"]) / SumNA(SampWt)) %>%
+  summarise(Proportion = SumNA(SampWt[Sex == "F"]) / 
+              (SumNA(SampWt[Sex == "F"]) + SumNA(SampWt[Sex == "M"]))) %>%
   ungroup() %>%
   complete(Year = yrRange) %>%
   arrange(Year)
