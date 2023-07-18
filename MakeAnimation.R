f5eb84b2e494c7789b29575171fc94bef60807ec
Grinnell <Matthew.Grinnell@dfo-mpo.gc.ca>
2023-07-18 13:22:42 -0700

    Create MakeAnimation.R
    Small script to make the animation in a 64-bit shell (was running out of memory in 32-bit R)

---
+++ MakeAnimation.R
@@ -0,0 +1,12 @@
+# Load the saved image
+load(file = file.path(paste("Temp", ".RData", sep = "")))
+# Load required packages
+UsePackages(pkgs = c(
+  "tidyverse", "RODBC", "zoo", "Hmisc", "scales", "sp", "maptools", "rgdal",
+  "rgeos", "raster", "xtable", "cowplot", "grid", "colorRamps", "RColorBrewer",
+  "stringr", "lubridate", "readxl", "plyr", "ggforce", "viridis", "ggthemes",
+  "SpawnIndex", "tidyselect", "ggrepel"
+))
+# Make the animation (twice to remove empty first page)
+PlotLocationsYear(dat = siYearLoc)
+PlotLocationsYear(dat = siYearLoc)
